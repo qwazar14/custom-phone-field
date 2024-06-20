8636ae5b5b429a0d832827f5fa4ccbf793089a47
qwazar14 <zenez89@gmail.com>
2024-06-20 09:40:34 +0300

    1.1

--- custom-phone-field/custom-phone-field.php
+++ custom-phone-field/custom-phone-field.php
@@ -18,7 +18,7 @@ add_filter('woocommerce_checkout_fields', 'add_custom_phone_field');
 function add_custom_phone_field($fields) {
     $fields['billing']['billing_phone_ua'] = array(
         'type'        => 'tel',
-        'label'       => __('Phone Number (UA)', 'woocommerce'),
+        'label'       => __('Телефон', 'woocommerce'),
         'placeholder' => _x('+380-xx-xxx-xx-xx', 'placeholder', 'woocommerce'),
         'required'    => true,
         'class'       => array('form-row-wide'),
@@ -36,5 +36,15 @@ function enqueue_custom_phone_field_scripts() {
     if (is_checkout()) {
         wp_enqueue_script('jquery-mask-plugin', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js', array('jquery'), '1.14.16', true);
         wp_enqueue_script('custom-phone-field-script', plugins_url('custom-phone-field.js', __FILE__), array('jquery-mask-plugin'), '1.0', true);
+    }
+}
+
+// Save custom phone field without dashes
+add_action('woocommerce_checkout_update_order_meta', 'save_custom_phone_field');
+
+function save_custom_phone_field($order_id) {
+    if (isset($_POST['billing_phone_ua'])) {
+        $phone = str_replace('-', '', sanitize_text_field($_POST['billing_phone_ua']));
+        update_post_meta($order_id, '_billing_phone_ua', $phone);
     }
 }
