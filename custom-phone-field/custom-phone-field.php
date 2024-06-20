980e64d77ad0ed6628076be585fc1ce552d71434
qwazar14 <zenez89@gmail.com>
2024-06-20 09:51:03 +0300

    1.2

--- custom-phone-field/custom-phone-field.php
+++ custom-phone-field/custom-phone-field.php
@@ -2,9 +2,10 @@
 /*
 Plugin Name: Пользовательское поле телефона для WooCommerce
 Description: Добавляет пользовательское поле телефона с маской укр номера на страницу оформления заказа WooCommerce.
-Version: 1.1
+Version: 1.2
 Author: Maksym "Qwazar" Mezhyrytskyi
 Author URI: https://github.com/qwazar14/
+Plugin URI: https://github.com/qwazar14/custom-phone-field
 */
 
 // Exit if accessed directly
@@ -46,5 +47,24 @@ function save_custom_phone_field($order_id) {
     if (isset($_POST['billing_phone_ua'])) {
         $phone = str_replace('-', '', sanitize_text_field($_POST['billing_phone_ua']));
         update_post_meta($order_id, '_billing_phone_ua', $phone);
+        update_post_meta($order_id, '_billing_phone', $phone);
     }
+}
+
+// Add custom phone field to order edit page
+add_filter('woocommerce_admin_billing_fields', 'add_custom_phone_to_admin_order', 10, 1);
+
+function add_custom_phone_to_admin_order($fields) {
+    $fields['phone_ua'] = array(
+        'label' => __('Телефон', 'woocommerce'),
+        'show' => true
+    );
+    return $fields;
+}
+
+// Display custom phone field in admin order page
+add_action('woocommerce_admin_order_data_after_billing_address', 'display_custom_phone_in_admin_order', 10, 1);
+
+function display_custom_phone_in_admin_order($order){
+    echo '<p><strong>'.__('Телефон').':</strong> ' . get_post_meta($order->get_id(), '_billing_phone_ua', true) . '</p>';
 }
